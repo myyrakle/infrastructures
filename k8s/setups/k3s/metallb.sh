5b6da2a48c6844fe88663df8656eeafaf820944d
myyrakle <sssang97@naver.com>
2026-02-03 14:51:48 +0900

    Allow firewall for MetalLB multi-node setup
    Added firewall rules for multi-node cluster support.

--- k8s/setups/k3s/metallb.sh
+++ k8s/setups/k3s/metallb.sh
@@ -3,4 +3,8 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.12/conf
 vi L2-range-allocation.yaml
 kubectl apply -f ./L2-range-allocation.yaml
 
+# If multi node cluster
+sudo ufw allow 7946/tcp
+sudo ufw allow 7946/udp
+
 # Ref: https://blog.naver.com/sssang97/223330537493
