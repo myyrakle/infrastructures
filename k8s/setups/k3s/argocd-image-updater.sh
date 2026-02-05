bdda9a85f8b5e7c7e1ff3ed13a4cf35d09398851
myyrakle <sssang97@naver.com>
2026-02-05 14:00:13 +0900

    Patch argocd-image-updater with AWS settings
    Add AWS region environment variables and update configmap for ECR.

--- k8s/setups/k3s/argocd-image-updater.sh
+++ k8s/setups/k3s/argocd-image-updater.sh
@@ -10,3 +10,45 @@ vim image-updater-values.yaml
 helm upgrade argocd-image-updater argo/argocd-image-updater \
   -n argocd \
   -f image-updater-values.yaml
+
+
+kubectl patch deployment argocd-image-updater-controller -n argocd --type='json' -p='[
+  {
+    "op": "add",
+    "path": "/spec/template/spec/containers/0/envFrom",
+    "value": [
+      {
+        "secretRef": {
+          "name": "k8s-ecr-login-renew-aws-secret"
+        }
+      }
+    ]
+  },
+  {
+    "op": "add",
+    "path": "/spec/template/spec/containers/0/env/-",
+    "value": {
+      "name": "AWS_DEFAULT_REGION",
+      "value": "ap-northeast-2"
+    }
+  },
+  {
+    "op": "add",
+    "path": "/spec/template/spec/containers/0/env/-",
+    "value": {
+      "name": "AWS_REGION",
+      "value": "ap-northeast-2"
+    }
+  }
+]'
+
+kubectl patch configmap argocd-image-updater-config -n argocd --type='merge' -p='
+data:
+  registries.conf: |
+    registries:
+    - name: ECR
+      prefix: 209863623098.dkr.ecr.ap-northeast-2.amazonaws.com
+      api_url: https://209863623098.dkr.ecr.ap-northeast-2.amazonaws.com
+      credentials: pullsecret:default/k8s-ecr-login-renew-docker-secret
+      default: true
+'
