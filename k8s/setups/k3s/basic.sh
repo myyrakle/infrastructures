b3ac7ad22960f92d70b3ca45d6651d5982113635
myyrakle <sssang97@naver.com>
2026-02-08 01:58:00 +0900

    Disable UFW and update K3S installation script
    Disabled UFW firewall for master and slave nodes and updated K3S installation commands.

--- k8s/setups/k3s/basic.sh
+++ k8s/setups/k3s/basic.sh
@@ -1,13 +1,11 @@
 # Master
+sudo ufw disable
 curl -sfL https://get.k3s.io | K3S_NODE_NAME=노드명 sh -
-sudo ufw allow 6443/tcp 
-sudo ufw allow 10250/tcp
-sudo ufw allow 2379:2380/tcp
-sudo ufw allow 8472/udp 
-sudo ufw allow 30000:32767/tcp
+
 
 # If need join
 sudo cat /var/lib/rancher/k3s/server/node-token 
 
 # Slaves
+sudo ufw disable
 curl -sfL https://get.k3s.io | K3S_NODE_NAME=노드명 K3S_URL=https://myserver:6443 K3S_TOKEN=mynodetoken sh -
