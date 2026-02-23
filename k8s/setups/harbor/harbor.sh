0ac3915b1c1596db1bee12d14f3927746edcd206
myyrakle <sssang97@naver.com>
2026-02-23 18:50:14 +0900

    Add Docker registry secret creation for Harbor
    Added commands to create a Docker registry secret for Harbor.

--- k8s/setups/harbor/harbor.sh
+++ k8s/setups/harbor/harbor.sh
@@ -9,3 +9,10 @@ kubectl get pods -n harbor -o wide
 
 kubectl patch svc harbor -n harbor --type=json -p='[{"op": "replace", "path": "/spec/ports", "value": [{"name":"default", "port":17000, "targetPort": 8080, "protocol": "TCP"}]}]'
 kubectl patch svc harbor -n harbor -p '{"spec": {"type":"ClusterIP", "externalIPs": ["192.168.0.8"]}}'
+
+# ... 
+kubectl create secret docker-registry harbor-secret \
+  --namespace default \
+  --docker-server=192.168.0.8:17000 \
+  --docker-username=admin \
+  --docker-password=<password>
